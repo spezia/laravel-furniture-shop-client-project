ec095bf5097fe968895b1e63eea2ba766846d731
Alek Rancic <aleks.rancic@gmail.com>
2020-12-19 12:36:57 +0100

    Fix active class issue in menu

--- web/resources/views/components/menu.blade.php
+++ web/resources/views/components/menu.blade.php
@@ -5,18 +5,20 @@
         <a class="nav-link" href="{{ route('pages.front.home') }}">{{ __('custom.home') }}</a>
         <div class="active-line"></div>
     </li>
-    <li class="nav-item  @if(\Request::is('about')) active @endif">
+    <li class="nav-item  @if(\Request::route()->getName() == 'page.about') active @endif">
         <a class="nav-link" href="{{ route('page.about') }}">{{ __('custom.aboutus') }}</a>
     </li>
-    <li class="nav-item has-dropdown @if(\Request::is('products*')) active @endif">
+    <li class="nav-item has-dropdown 
+        @if(in_array(\Request::route()->getName(), ['products.front.home', 'products.front.show', 'categories.front.show'])) active @endif
+    ">
         <a class="nav-link" href="{{ route('products.front.home') }}">{{ __('custom.products') }}</a>
         <x-categories/>
     </li>
     <li
-        class="nav-item @if(\Request::is('impressions')) active @endif">
+        class="nav-item @if(\Request::route()->getName() == 'page.impressions') active @endif">
         <a class="nav-link" href="{{ route('page.impressions') }}">{{ __('custom.impressions') }}</a>
     </li>
-    <li class="nav-item @if(\Request::is('contact')) active @endif">
+    <li class="nav-item @if(\Request::route()->getName() == 'page.contact') active @endif">
         <a class="nav-link" href="{{ route('page.contact') }}">{{ __('custom.contact') }}</a>
     </li>
 
