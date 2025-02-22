2323bb1b1769614b8dc956db7807adf6cecf7644
alek rancic <aleks.rancic@gmail.com>
2025-02-22 23:31:54 +0100

    Small docker fix

--- web/start.sh
+++ web/start.sh
@@ -5,6 +5,8 @@ docker-compose up --detach --build
 # Install dependencies
 docker-compose exec app composer install --ignore-platform-reqs --no-ansi --no-interaction --no-scripts --no-suggest --no-progress --prefer-dist
 
+docker cp ./docker/env.conf app:/var/www/html/.env
+
 # Set correct permissions
 docker-compose exec app chown -R www-data:www-data /var/www/html/storage /var/www/html/bootstrap/cache
 docker-compose exec app chmod -R 775 /var/www/html/storage /var/www/html/bootstrap/cache
